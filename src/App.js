84075188c8c2b2b28850e55368d3d81804813f39
Aman11b <amansingh1100b@gmail.com>
2026-01-26 08:22:44 +0530

    creating state variables with useState

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,22 +7,25 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 2;
+  // const step = 2;
+  const [step, setStep] = useState(1);
 
   function handlePrevious() {
     // alert("prev");
+    if (step > 1) setStep(step - 1);
   }
 
   function handleNext() {
     // alert("next");
+    if (step < 3) setStep(step + 1);
   }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
       <p className="message">
         Step {step}:{messages[step - 1]}
@@ -43,3 +48,15 @@ export default function App() {
     </div>
   );
 }
+
+/**
+ * STATE
+ * -> data a component can hold over time, ifo that component needs to remember though its life cycle
+ * -> its kindoff memory that can be changed
+ * -> state variable/peice of state a single variable in a component
+ * -> state is an entrire condition at ant point of time > all peices ofm state together
+ * #-> updating state re-render component (one single component render its called a view all views together makes final UI)
+ * -> with state react keep UI sync with data
+ * ->useState retusn a array it takes defaut value and a function
+ * -> useState is called hooks(but only in top level of function not in if or loop or something else)
+ */
